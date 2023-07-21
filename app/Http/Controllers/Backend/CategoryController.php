f05618836c7f5f901bdf2338e0f9304d64cb73e8
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-07-21 10:38:28 +0700

    Update CategoryController.php :rocket:

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -7,5 +7,33 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        # code...
+    }
+
+    public function store(Request $request)
+    {
+        return $request->all();
+    }
+
+    public function show($id)
+    {
+        # code...
+    }
+
+    public function edit($id)
+    {
+        # code...
+    }
+
+    public function update(Request $request,$id)
+    {
+        return $request->all();
+    }
+
+    public function destroy($id)
+    {
+        # code...
+    }
 }
