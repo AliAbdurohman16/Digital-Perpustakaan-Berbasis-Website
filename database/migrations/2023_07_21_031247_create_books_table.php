4c51d4f4d94f9458530082337cd4310a70b97180
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-07-21 10:25:00 +0700

    Update Models and migrations :rocket:

---
+++ database/migrations/2023_07_21_031247_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('cover');
+            $table->string('title');
+            $table->unsignedBigInteger('category_id');
+            $table->text('description');
+            $table->integer('amount')->default(12);
+            $table->string('file_book');
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
