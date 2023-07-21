a79fae836e59e192c1be00bbca94f0d8c2ba902b
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-07-21 10:34:27 +0700

    Update routes :rocket:

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,11 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('dashboard', [Backend\DashboardController::class, 'index'])->name('dashboard');
+Route::resources([
+    'categories', Backend\CategoryController::class,
+    'books', Backend\BookController::class,
+]);
