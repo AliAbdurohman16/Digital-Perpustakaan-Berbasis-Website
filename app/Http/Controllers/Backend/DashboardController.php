54dcd03293f915021739800e1f6ba81fce25325f
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-01-01 14:18:34 +0700

    adding view dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Book;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.dashboard.index');
+        $data = [
+            'category' => Category::count(),
+            'book' => Book::count(),
+            'user' => User::count(),
+        ];
+
+        return view('backend.dashboard.index', $data);
     }
 }
